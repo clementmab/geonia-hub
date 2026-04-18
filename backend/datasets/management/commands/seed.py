7a194f17461ee921040a130e6134b1559738c043
Clementmab <mabikashalmar@gmail.com>
2026-04-18 12:01:19 +0100

    Fix UserProfile import in seed.py and add users migration

--- backend/datasets/management/commands/seed.py
+++ backend/datasets/management/commands/seed.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from datasets.models import Category, Department, Dataset
+from users.models import UserProfile
 
 
 class Command(BaseCommand):
