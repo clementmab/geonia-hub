ad9dabd439d68534e7fe2eb70ee452b13e68135a
Clementmab <mabikashalmar@gmail.com>
2026-04-19 13:27:46 +0100

    Update logo image path to GeoNiaa.jpg
    - Remplacer Geonia.jpg par GeoNiaa.jpg pour correspondre au fichier renommé

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import logoImage from '../images/Geonia.jpg';
+import logoImage from '../images/GeoNiaa.jpg';
 
 const NAV_GREEN = '#0F6E56';
 
